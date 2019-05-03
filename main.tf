b1ce46b51087b356a9fcddc2e9a2e581de67ae2b
eelrb <eelrb@yahoo.com>
2019-05-03 01:33:59 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------------
+// Variables
+
+
+
+//--------------------------------------------------------------------
+// Modules
+module "vm" {
+  source  = "app.terraform.io/bleehashiorg/vm/azure"
+  version = "0.0.1"
+
+  network_interface_id = "${module.vpc.network_interface_id}"
+  resource_group_name = "${module.vpc.resource_group_name}"
+}
+
+module "vpc" {
+  source  = "app.terraform.io/bleehashiorg/vpc/azure"
+  version = "0.0.4"
+
+  resource_group_name = "myrg"
+}
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "vm_public_ip" {
+
+value = "${module.vpc.vm_public_ip}"
+}
